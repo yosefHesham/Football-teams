6abb272706f42aa5c29acde5a0840e871d8650ca
yosefHesham <yousefmobdev@gmail.com>
2022-07-02 02:03:01 +0200

    Create action test

---
+++ src/tests/actions.test.js
@@ -0,0 +1,7 @@
+import { fetchLeaguesAction, FETCH_LEAGUES } from "../redux/teams/teams_reducer";
+
+it("should return proper action object", () => {
+  const leagues= ["premier league","la liga",];
+
+  expect(fetchLeaguesAction(leagues)).toStrictEqual({type:FETCH_LEAGUES,leagues})
+})
